6bdc3f86161b85466563e1fb96fcc02ef210a525
Sarthak <sarthakgohel5555@gmail.com>
2024-09-10 00:08:20 -0400

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/header/Header";
 
 function App() {
     return (
-        <div>
+        <div className="font-bodyFont">
             <Header />
         </div>
     );
